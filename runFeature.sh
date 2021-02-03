1341b0b91064aace7f6b8073ecffdb0e36ab99a7
Edward Jezisek <ejezisek@yahoo.com>
2021-02-03 11:33:07 -0500

    More updates.

--- runFeature.sh
+++ runFeature.sh
@@ -1,8 +1,10 @@
-pwd
-node node_modules/@ln-maf/preprocessor/exec.js  packages/$* --packageLocation test
-cd packages/$*
-node ../../node_modules/@cucumber/cucumber/bin/cucumber-js  cucumber-js --require "test/**/*.js" tmp/test 
-result=$?
-cd -
-exit $result
+
+if [[ "$ENVIRONMENT" == "CI" ]]; then
+  bash runFeatureCI.sh $*
+elif [[ "$ENVIRONMENT" == "COVERAGE" ]]; then
+  bash runFeatureCoverage.sh $*
+else
+  bash runFeatureHTML.sh $*
+fi
+  exit $?
 
--- runFeatureHTML.sh
+++ runFeatureHTML.sh
@@ -1,4 +1,3 @@
-pwd
 node node_modules/@ln-maf/preprocessor/exec.js  packages/$* --packageLocation test
 cd packages/$*
 node ../../node_modules/@cucumber/cucumber/bin/cucumber-js  --format json cucumber-js --require "test/**/*.js" tmp/test > ../../test/report/$*.json
