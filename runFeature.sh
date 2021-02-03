fd05bdf7dffe132117d12d25bb71fdee6effac2c
ejezisek <ejezisek@yahoo.com>
2021-02-02 20:54:25 -0500

    Moved tests to ensure it's recognized by tools looking for test. (#5)

--- runFeature.sh
+++ runFeature.sh
@@ -1,5 +1,5 @@
 mkdir -p ./test/report
-node node_modules/@ln-maf/preprocessor/exec.js  packages/$*
-node node_modules/@cucumber/cucumber/bin/cucumber-js --require "packages/$*/features/**/*.js" packages/$*/tmp/features 
+node node_modules/@ln-maf/preprocessor/exec.js  packages/$* --packageLocation test
+node node_modules/@cucumber/cucumber/bin/cucumber-js --require "packages/$*/test/**/*.js" packages/$*/tmp/test 
 result=$?
 exit $result
