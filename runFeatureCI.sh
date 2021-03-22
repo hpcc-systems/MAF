590ffde923fa3e9d55e7dcfcd2f94792f66deebf
Edward Jezisek <ejezisek@yahoo.com>
2021-03-22 18:36:22 -0400

    See if this fixes ci issues.

--- runFeatureCI.sh
+++ runFeatureCI.sh
@@ -2,7 +2,7 @@ pwd
 node node_modules/@ln-maf/preprocessor/exec.js  packages/$1 --packageLocation test
 cd packages/$1
 shift
-npx cucumber-js $*  --require "test/**/*.js" tmp/test 
+node ../../node_modules/@cucumber/cucumber/bin/cucumber-js  cucumber-js $* --require "test/**/*.js" tmp/test 
 result=$?
 cd -
 exit $result
