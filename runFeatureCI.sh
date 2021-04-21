f371b08da82a3d5d46837a465a7701be8bdfb247
Edward Jezisek <ejezisek@yahoo.com>
2021-04-21 17:18:05 -0400

    Fixes to CI script.

--- runFeatureCI.sh
+++ runFeatureCI.sh
@@ -2,7 +2,7 @@ pwd
 npx preprocessor packages/$1 --packageLocation test
 cd packages/$1
 shift
-node node_modules/@ln-maf/preprocessor/exec.js $*  --require "test/**/*.js" tmp/test 
+node ../../node_modules/@cucumber/cucumber/bin/cucumber-js  cucumber-js $* --require "test/**/*.js" tmp/test
 result=$?
 cd -
 exit $result
