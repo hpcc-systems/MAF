1eae0f51bcbcf3ceeb2ea7ccfca2dfe666723263
Edward Jezisek <ejezisek@yahoo.com>
2021-06-01 12:06:50 -0400

    Add extras variable for html tests.

--- runFeatureHTML.sh
+++ runFeatureHTML.sh
@@ -1,6 +1,6 @@
 npx preprocessor  packages/$* --packageLocation test
 cd packages/$*
-node ../../node_modules/@cucumber/cucumber/bin/cucumber-js  -f json:../../test/report/$*.json cucumber-js --require "test/**/*.js" tmp/test 
+node ../../node_modules/@cucumber/cucumber/bin/cucumber-js  $EXTRAS -f json:../../test/report/$*.json cucumber-js --require "test/**/*.js" tmp/test 
 result=$?
 cd -
 node node_modules/@ln-maf/core/multiReport.js
