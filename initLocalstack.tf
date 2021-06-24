2cd2ae889deddfbb525ae4bf4051409695f3fcbe
Dominick Martelly <DMartelly@users.noreply.github.com>
2021-06-24 09:41:38 -0400

    Add terraform to AWS testing using localstack (#18)
    * adding init terraform
    
    * added terraform to github workflow
    
    * forcing a build
    
    * added terraform apply to yml
    
    * remove bash scripts
    
    * remove range key and added sqs
    
    * fix sqs setting in terraform

---
+++ initLocalstack.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  profile                     = "default"
+  region                      = "us-east-1"
+  access_key                  = "1234"
+  secret_key                  = "xyz"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  s3_force_path_style         = true
+
+  endpoints {
+    dynamodb = "http://localhost:4569"
+    s3       = "http://localhost:4572"
+    sqs       = "http://localhost:4576"
+  }
+}
+
+resource "aws_s3_bucket" "test-bucket" {
+  bucket = "testScriptbucket"
+
+}
+
+resource "aws_dynamodb_table" "test-table" {
+  name           = "testtable"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 2
+  write_capacity = 2
+  hash_key       = "label"
+  attribute {
+    name = "label"
+    type = "S"
+  }
+}
+
+resource "aws_sqs_queue" "testQueue1" {
+  name = "testQueue"
+}
+
+resource "aws_sqs_queue" "testQueue2" {
+  name = "testQueue2"
+}
