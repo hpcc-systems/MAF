e52d45d57076517077e89e7cb326df9b82927576
Dominick Martelly <dominick.martelly@lexisnexisrisk.com>
2022-02-23 10:36:10 -0500

    updated ecs run task

--- packages/aws/stepDefinitions/ecs.js
+++ packages/aws/stepDefinitions/ecs.js
@@ -13,7 +13,7 @@ MAFWhen('run task {string}', function (taskDefinition) {
  * @param {Array} additionalArgs pairs of strings that will be added to the aws cli
  * @return {JSON} The task information
  */
-function runTask (taskDefinition, additionalArgs) {
+function runTask(taskDefinition, additionalArgs) {
   const args = ['ecs', 'run-task']
   if (!taskDefinition || taskDefinition === '') {
     throw new Error("The 'taskDefinition' for ecs run-task is required")
@@ -26,9 +26,9 @@ function runTask (taskDefinition, additionalArgs) {
   this.attach(JSON.stringify({ lastRun: this.results.lastRun }, null, 2))
 }
 
-Given('ECS TaskDefinition {string} does not exists', function (taskDefinition) {
+Given('ECS TaskDefinition {string} does not exist', function (taskDefinition) {
   if (taskDefinitionExists(taskDefinition)) {
-    throw new Error('ECS Task Definition  ' + taskDefinition + ' does exists')
+    throw new Error('ECS Task Definition  ' + taskDefinition + ' does exist')
   }
 })
 
@@ -54,17 +54,105 @@ MAFWhen('Registered TaskDefinition {string} is retrieved', function (taskFamilyN
   const res = runAWS('ecs list-task-definitions --sort DESC --profile ' + profile + ' --family-prefix ' + taskFamilyName + ' ')
   const response = JSON.parse(res.stdout)
   return response.taskDefinitionArns
-
 })
+
 MAFWhen('a new ecs task for {string} taskDefinition for version {string} is executed in cluster {string} with FARGATE as launch-type having {string} subnets and {string} subgroups', function (taskDef, version, clusterName, subnets, subgroups) {
   const res = runAWS('ecs run-task --cluster ' + clusterName + ' --task-definition ' + taskDef + ':' + version + ' --profile ' + profile + '  --launch-type FARGATE --enable-ecs-managed-tags --network-configuration awsvpcConfiguration={subnets=[' + subnets + '],securityGroups=[' + subgroups + '],assignPublicIp=DISABLED}')
   const response = JSON.parse(res.stdout)
   return get_new_created_task_Arns(response.tasks)
 })
 
-MAFWhen('perform ecs task with {jsonObject}', function (json) {
-  let payload = performJSONObjectTransform.call(this, json)
+/**
+ * Places an item on a dynamoDB table
+ * @param {Array} additionalArgs pairs of strings that will be added to the aws cli
+ * @return {JSON} The placed dynamodb item and its values
+ */
+function ecsRunTask (activeArgs, additionalArgs) {
+  const ecsArgs = {}
+  Object.assign(ecsArgs, this.results)
+  Object.assign(ecsArgs, activeArgs)
 
+  const args = ['ecs', 'run-task']
+  if (!ecsArgs.taskDefinition) {
+    throw new Error("The 'taskDefinition' for ecs run-task is required")
+  }
+  args.push('--task-definition', ecsArgs.taskDefinition)
+  if (!ecsArgs.cluster) {
+    throw new Error("The 'cluster' for ecs run-task is required since defaults are different for each account")
+  }
+  args.push('--cluster', ecsArgs.cluster)
+  if (ecsArgs.networkConfiguration) {
+    args.push(
+      '--network-configuration',
+      'awsvpcConfiguration={subnets=' + JSON.stringify(ecsArgs.networkConfiguration.subnets) + ',securityGroups=' + JSON.stringify(ecsArgs.networkConfiguration.securityGroups) + ',assignPublicIp=' + (ecsArgs.networkConfiguration.assignPublicIp ? ecsArgs.networkConfiguration.assignPublicIp : 'DISABLED') + '}'
+    )
+  }
+  if (ecsArgs.enableECSManagedTags) {
+    args.push('--enable-ecs-managed-tags')
+  }
+  args.push('--launch-type', ecsArgs.launchType ? ecsArgs.launchType : 'FARGATE')
+  if (additionalArgs) {
+    args.push(...additionalArgs)
+  }
+
+  this.attach(`Query: ${args}`)
+  this.results.lastRun = JSON.parse(runAWS(args).stdout.toString())
+  this.attach(JSON.stringify({ lastRun: this.results.lastRun }, null, 2))
+}
+
+/**
+ * Extracts variables for ecs run-task and preforms the aws cli command
+ * @param {JSON} payload an object containing keys / values for the run-task
+ */
+function performECSRunTaskFromJSON (payload) {
+  const activeArgs = {}
+  const additionalArgs = []
+  Object.keys(payload).forEach((key) => {
+    switch (key) {
+      case 'taskDefinition':
+        activeArgs[key] = payload[key]
+        break
+      case 'cluster':
+        activeArgs[key] = payload[key]
+        break
+      case 'networkConfiguration':
+        activeArgs[key] = payload[key]
+        break
+      case 'enableECSManagedTags':
+        activeArgs[key] = payload[key]
+        break
+      default:
+        additionalArgs.push('--' + key)
+        additionalArgs.push(payload[key])
+    }
+  })
+  ecsRunTask.call(this, activeArgs, additionalArgs)
+}
+
+/**
+ * Runs an ecs task from a provided JSONobject
+ */
+MAFWhen('ecs run-task from {jsonObject} is performed', function (payload) {
+  payload = performJSONObjectTransform.call(this, payload)
+  performECSRunTaskFromJSON.call(this, payload)
+})
+
+/**
+ * Performs an ecs task based on the provided docstring and variables defined in a document string
+ */
+MAFWhen('perform ecs run-task:', function (docString) {
+  const payload = JSON.parse(fillTemplate(docString, this.results))
+  performECSRunTaskFromJSON.call(this, payload)
+})
+
+/**
+ * Gets a query / item from a dynamoDB table
+ */
+MAFWhen('ecs run-task is performed', function () {
+  ecsRunTask.call(this)
+})
+
+MAFWhen('perform ecs run-task with {jsonObject}', function (json) {
   if (!checkReqParams(payload)) {
     return new Error("Missing attribute in payload.")
   }
@@ -130,34 +218,32 @@ function checkReqParams(payload) {
   return true
 }
 
-function taskDefinitionExists(taskdef) {
+function taskDefinitionExists (taskdef) {
   const res = runAWS('ecs list-task-definitions --sort DESC --family-prefix ' + taskdef)
   const taskDefinitionList = JSON.parse(res.stdout)
   const data = taskDefinitionList.taskDefinitionArns
   if (taskDefinitionList.taskDefinitionArns && taskDefinitionList.taskDefinitionArns === 0) {
     return false
   }
-  let isExists = false
   for (let step = 0; data.length; i++) {
     const taskIndividual = data[step].split(':task-definition/')
-    const itask = taskIndividual[1].split(':');
+    const itask = taskIndividual[1].split(':')
     if (taskdef === itask[0].trim()) {
-      isExists = true
-      break
+      return true
     }
   }
-  return isExists
+  return false
 }
 
 function isEmpty(str) {
-  return (!str || 0 === str.length);
+  return (!str || str.length === 0)
 }
 
 function clusterExists(clusterName) {
-  const res = runAWS('ecs list-clusters --profile ' + profile)
+  const res = runAWS('ecs list-clusters')
   const response = JSON.parse(res.stdout)
   for (let i = 0; i < response.clusterArns.length; i++) {
-    let clusterSplit = response.clusterArns[i].split(":cluster/")
+    const clusterSplit = response.clusterArns[i].split(':cluster/')
     if (clusterSplit.length > 0 && clusterName === clusterSplit[1]) {
       return true
     }
