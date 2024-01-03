2d0d86fbda2cb0078342e7dcd817180e8bb88eb5
Dominick Martelly <dominick.martelly@lexisnexisrisk.com>
2024-01-03 14:45:45 -0500

    Update ESLint configuration

--- packages/aws/.eslintrc.js
+++ packages/aws/.eslintrc.js
@@ -1,17 +1,26 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true
-  },
-  extends: [
-    'standard'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  },
-  rules: {
-    'no-eval': 'off',
-    'no-new-func': 'off'
-  }
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true
+    },
+    extends: [
+        'standard'
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest'
+    },
+    rules: {
+        indent: ['error', 4],
+        'space-before-function-paren': [
+            'error',
+            {
+                anonymous: 'always',
+                named: 'never',
+                asyncArrow: 'always'
+            }
+        ],
+        'no-eval': 'off',
+        'no-new-func': 'off'
+    }
 }
