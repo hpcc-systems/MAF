5fae02bd78e6936c2176f2420e8e46f34a386275
Dominick Martelly <dominick.martelly@lexisnexisrisk.com>
2024-02-07 16:31:17 -0500

    Add sftp package to package.json

--- packages/sftp/stepDefinitions/sftp.js
+++ packages/sftp/stepDefinitions/sftp.js
@@ -1,4 +1,4 @@
-const { MAFWhen, getFilePath, filltemplate } = require('@ln-maf/core')
+const { MAFWhen, getFilePath, fillTemplate } = require('@ln-maf/core')
 const { setDefaultTimeout } = require('@cucumber/cucumber')
 const { execSync } = require('child_process')
 const Client = require('ssh2-sftp-client')
@@ -15,13 +15,13 @@ const conf = {
 }
 
 MAFWhen('server {string} is set', function (name) {
-    name = filltemplate(name, this.results)
+    name = fillTemplate(name, this.results)
     conf.host = name
     return conf
 })
 
 MAFWhen('username {string} is set', function (name) {
-    name = filltemplate(name, this.results)
+    name = fillTemplate(name, this.results)
     conf.username = name
     return conf
 })
@@ -31,7 +31,7 @@ MAFWhen('a user send command {string} to server {string}', function (command, se
 })
 
 MAFWhen('list of files from remote server directory {string} is received', async function (serverDirectory) {
-    serverDirectory = filltemplate(serverDirectory, this.results)
+    serverDirectory = fillTemplate(serverDirectory, this.results)
     await sftp.connect(conf)
     const remoteFiles = await sftp.list(serverDirectory, item => item.type === '-')
     await sftp.end()
@@ -39,8 +39,8 @@ MAFWhen('list of files from remote server directory {string} is received', async
 })
 
 MAFWhen('list of files from remote server {string} directory {string} is received', async function (serverName, serverDirectory) {
-    conf.host = filltemplate(serverName, this.results)
-    serverDirectory = filltemplate(serverDirectory, this.results)
+    conf.host = fillTemplate(serverName, this.results)
+    serverDirectory = fillTemplate(serverDirectory, this.results)
     await sftp.connect(conf)
     const remoteFiles = await sftp.list(serverDirectory, item => item.type === '-')
     await sftp.end()
@@ -48,63 +48,56 @@ MAFWhen('list of files from remote server {string} directory {string} is receive
 })
 
 MAFWhen('file {string} is copied as user {string} from server {string}', function (serverFile, userName, serverName) {
-    serverFile = filltemplate(serverFile, this.results)
-    conf.username = filltemplate(userName, this.results)
-    conf.host = filltemplate(serverName, this.results)
+    serverFile = fillTemplate(serverFile, this.results)
+    conf.username = fillTemplate(userName, this.results)
+    conf.host = fillTemplate(serverName, this.results)
     return getFileFromServer(serverFile, getFilePath(path.basename(serverFile), this))
 })
 
 MAFWhen('file {string} is get from remote server to {string}', function (serverFile, localFile) {
-    serverFile = filltemplate(serverFile, this.results)
-    localFile = filltemplate(localFile, this.results)
+    serverFile = fillTemplate(serverFile, this.results)
+    localFile = fillTemplate(localFile, this.results)
     localFile = getFilePath(localFile, this)
     return getFileFromServer(serverFile, localFile)
 })
 
 MAFWhen('latest file as user {string} from server {string} in directory {string} is retrieved', async function (userName, serverName, directory) {
-    userName = filltemplate(userName, this.results)
-    serverName = filltemplate(serverName, this.results)
-    directory = filltemplate(directory, this.results)
+    userName = fillTemplate(userName, this.results)
+    serverName = fillTemplate(serverName, this.results)
+    directory = fillTemplate(directory, this.results)
     conf.host = serverName
     conf.username = userName
     return await copyLatestFileFromServer.call(this, directory)
 })
 
 MAFWhen('latest file as user {string} from server {string} in directory {string} is downloaded to {string}', async function (userName, serverName, directory, localPath) {
-    userName = filltemplate(userName, this.results)
-    serverName = filltemplate(serverName, this.results)
-    directory = filltemplate(directory, this.results)
-    localPath = filltemplate(localPath, this.results)
+    userName = fillTemplate(userName, this.results)
+    serverName = fillTemplate(serverName, this.results)
+    directory = fillTemplate(directory, this.results)
+    localPath = fillTemplate(localPath, this.results)
     conf.host = serverName
     conf.username = userName
     return await copyLatestFileFromServer.call(this, directory, localPath)
 })
 
 MAFWhen('latest file as user {string} is read from server {string} in directory {string}', async function (userName, serverName, directory) {
-    userName = filltemplate(userName, this.results)
-    serverName = filltemplate(serverName, this.results)
-    directory = filltemplate(directory, this.results)
+    userName = fillTemplate(userName, this.results)
+    serverName = fillTemplate(serverName, this.results)
+    directory = fillTemplate(directory, this.results)
     conf.host = serverName
     conf.username = userName
     return await readLatestFileFromServer.call(this, directory)
 })
 
 MAFWhen('latest file in remote directory {string} is retrieved', async function (remoteDirectory) {
-    remoteDirectory = filltemplate(remoteDirectory, this.results)
+    remoteDirectory = fillTemplate(remoteDirectory, this.results)
     return await copyLatestFileFromServer.call(this, remoteDirectory)
 })
 
-MAFWhen('second to last file as user {string} from server {string} in directory {string} is retrieved', async function (userName, serverName, directory) {
-    userName = filltemplate(userName, this.results)
-    conf.host = filltemplate(serverName, this.results)
-    directory = filltemplate(directory, this.results)
-    return await copySecondLatestFileFromServer.call(this, directory)
-})
-
 MAFWhen('a user puts file {string} on server {string} to folder {string}', function (file, server, serverDirectory) {
-    file = filltemplate(file, this.results)
-    conf.host = filltemplate(server, this.results)
-    serverDirectory = filltemplate(serverDirectory, this.results)
+    file = fillTemplate(file, this.results)
+    conf.host = fillTemplate(server, this.results)
+    serverDirectory = fillTemplate(serverDirectory, this.results)
     if (!serverDirectory.endsWith('/')) {
         serverDirectory += '/'
     }
@@ -112,10 +105,10 @@ MAFWhen('a user puts file {string} on server {string} to folder {string}', funct
 })
 
 MAFWhen('a user puts file {string} on server {string} to folder {string} from local path {string}', function (file, server, serverDirectory, localPath) {
-    file = filltemplate(file, this.results)
-    conf.host = filltemplate(server, this.results)
-    serverDirectory = filltemplate(serverDirectory, this.results)
-    localPath = filltemplate(localPath, this.results)
+    file = fillTemplate(file, this.results)
+    conf.host = fillTemplate(server, this.results)
+    serverDirectory = fillTemplate(serverDirectory, this.results)
+    localPath = fillTemplate(localPath, this.results)
     if (!serverDirectory.endsWith('/')) {
         serverDirectory += '/'
     }
@@ -123,10 +116,10 @@ MAFWhen('a user puts file {string} on server {string} to folder {string} from lo
 })
 
 MAFWhen('a user {string} puts file {string} on server {string} to folder {string}', function (user, file, server, serverDirectory) {
-    file = filltemplate(file, this.results)
-    conf.user = filltemplate(user, this.results)
-    conf.host = filltemplate(server, this.results)
-    serverDirectory = filltemplate(serverDirectory, this.results)
+    file = fillTemplate(file, this.results)
+    conf.user = fillTemplate(user, this.results)
+    conf.host = fillTemplate(server, this.results)
+    serverDirectory = fillTemplate(serverDirectory, this.results)
     if (!serverDirectory.endsWith('/')) {
         serverDirectory += '/'
     }
@@ -134,11 +127,11 @@ MAFWhen('a user {string} puts file {string} on server {string} to folder {string
 })
 
 MAFWhen('a user {string} puts file {string} on server {string} to folder {string} from local path {string}', function (user, file, server, serverDirectory, localPath) {
-    file = filltemplate(file, this.results)
-    conf.user = filltemplate(user, this.results)
-    conf.host = filltemplate(server, this.results)
-    serverDirectory = filltemplate(serverDirectory, this.results)
-    localPath = filltemplate(localPath, this.results)
+    file = fillTemplate(file, this.results)
+    conf.user = fillTemplate(user, this.results)
+    conf.host = fillTemplate(server, this.results)
+    serverDirectory = fillTemplate(serverDirectory, this.results)
+    localPath = fillTemplate(localPath, this.results)
     if (!serverDirectory.endsWith('/')) {
         serverDirectory += '/'
     }
@@ -146,9 +139,9 @@ MAFWhen('a user {string} puts file {string} on server {string} to folder {string
 })
 
 MAFWhen('file {string} is put on remote server to {string}', async function (localFile, remoteFile) {
-    localFile = filltemplate(localFile, this.results)
+    localFile = fillTemplate(localFile, this.results)
     localFile = getFilePath(localFile, this)
-    remoteFile = filltemplate(remoteFile, this.results)
+    remoteFile = fillTemplate(remoteFile, this.results)
     if (!fs.existsSync(localFile)) {
         throw Error('File "' + localFile + '" doesn\'t exist')
     }
@@ -157,14 +150,14 @@ MAFWhen('file {string} is put on remote server to {string}', async function (loc
 })
 
 MAFWhen('file {string} is put on remote server {string} to {string}', async function (localFile, remoteServer, remoteFile) {
-    localFile = filltemplate(localFile, this.results)
+    localFile = fillTemplate(localFile, this.results)
     localFile = getFilePath(localFile, this)
-    conf.host = filltemplate(remoteServer, this.results)
-    remoteFile = filltemplate(remoteFile, this.results)
+    conf.host = fillTemplate(remoteServer, this.results)
+    remoteFile = fillTemplate(remoteFile, this.results)
     if (!fs.existsSync(localFile)) {
         throw Error('File "' + localFile + '" doesn\'t exist')
     }
-    remoteFile = filltemplate(remoteFile, this.results)
+    remoteFile = fillTemplate(remoteFile, this.results)
     await putFileToServer.call(this, localFile, remoteFile)
     return remoteFile
 })
@@ -263,32 +256,4 @@ async function readLatestFileFromServer(serverDirectory) {
     const remoteFileContents = await sftp.get(serverDirectory + latestRemoteFile)
     await sftp.end()
     return remoteFileContents.toString()
-}
-
-/**
- * Copies the 2nd latest file from a server's directory to the local directory
- *
- * @param {string} serverDirectory The linux path on the remote containing the 2nd latest file
- * @returns {string} The name of the file downloaded
- */
-async function copySecondLatestFileFromServer(serverDirectory, localPath) {
-    if (!serverDirectory.endsWith('/')) {
-        serverDirectory = serverDirectory + '/'
-    }
-    if (localPath && !localPath.endsWith('/')) {
-        localPath = localPath + '/'
-    }
-    await sftp.connect(conf)
-    const remoteFiles = await sftp.list(serverDirectory, item => item.type === '-')
-    if (remoteFiles.length <= 1) {
-        throw Error(`One or no files found on ${conf.host}:${serverDirectory}`)
-    }
-    remoteFiles.sort((a, b) => a.modifyTime - b.modifyTime)
-    const secondLatestRemoteFile = remoteFiles.at(-2).name
-    if (!localPath) {
-        localPath = getFilePath('', this)
-    }
-    await sftp.fastGet(serverDirectory + secondLatestRemoteFile, localPath + secondLatestRemoteFile)
-    await sftp.end()
-    return secondLatestRemoteFile
 }
