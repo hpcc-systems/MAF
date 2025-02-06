25ea11b9e433e3fbb2939a1d8e25b400db278ffd
Dominick Martelly <97986187+martdo02@users.noreply.github.com>
2025-02-06 10:37:03 -0500

    feat: enhance platform detection in multiReport.js and improve report metadata

--- packages/core/multiReport.js
+++ packages/core/multiReport.js
@@ -1,30 +1,36 @@
 #!/usr/bin/env node
 const report = require('multiple-cucumber-html-reporter')
 const os = require('os')
-const platformMap = {
-  darwin: 'osx'
 
+// Get the platform name from the os module
+// Possible values are `'aix'`, `'darwin'`, `'freebsd'`, `'linux'`, `'openbsd'`, `'sunos'`, and `'win32'`.
+const platformMap = {
+    darwin: 'osx',
+    win32: 'windows',
+    freebsd: 'linux',
+    openbsd: 'linux'
 }
 let platformName = os.platform()
 if (platformMap[os.platform()]) {
-  platformName = platformMap[os.platform()]
+    platformName = platformMap[os.platform()]
 }
 
+// Generate the report
 report.generate({
-  displayDuration: true,
-  metadata: {
-    device: 'Local test machine',
-    platform: {
-      name: platformName,
-      version: os.release()
-    }
-  },
-  customData: {
-    title: 'Run info',
-    data: [
-      { label: 'Report Generated:', value: `${new Date()}` }
-    ]
-  },
-  jsonDir: './test/report/',
-  reportPath: 'test/report/'
+    displayDuration: true,
+    metadata: {
+        device: os.hostname(),
+        platform: {
+            name: platformName,
+            version: os.release()
+        }
+    },
+    customData: {
+        title: 'Run info',
+        data: [
+            { label: 'Report Generated:', value: `${new Date()}` }
+        ]
+    },
+    jsonDir: './test/report/',
+    reportPath: 'test/report/'
 })
